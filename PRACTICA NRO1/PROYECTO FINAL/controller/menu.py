927eac0a968dea467fe7d8aae8ba8f05e14b70be
Abel30s <abelulises245@gmail.com>
2025-02-18 04:32:53 +0000

    PROYECTO FINAL

---
+++ PRACTICA NRO1/PROYECTO FINAL/controller/menu.py
@@ -0,0 +1,20 @@
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich.prompt import Prompt
+
+def menu(app:App):
+    console = Console()
+
+    while True:
+        # Crear el texto del menú con colores y emojis
+        menu_text = Text()
+        menu_text.append("\n📊 [bold cyan]Proyecto Datux[/bold cyan]\n", style="underline bold")
+        menu_text.append("\n[1] 🟢 Ingestar Data\n", style="green")
+        menu_text.append("[2] 📈 Reporte de Ventas\n", style="blue")
+        menu_text.append("[3] ❌ Salir\n", style="red")
+
+         console.print(Panel(menu_text, title="🚀 [bold magenta]Menú Principal[/bold magenta]", expand=False, border_style="yellow"))
+          opcion = Prompt.ask("[bold yellow]Selecciona una opción[/bold yellow]", choices=["1", "2", "3"], default="3")
+
+pass
---
+++ PRACTICA NRO1/PROYECTO FINAL/main.py
@@ -0,0 +1,15 @@
+from controller.menu import *
+if __name__ =="__main__":
+
+    menu()
+    pass 
+if opcion == "1":
+            IngestDataProducts(app)
+            pass
+           
+        elif opcion == "2":
+            GenerateReportVentas(app)
+            
+        elif opcion == "3":
+            pass
+            break  
